6cc4cd0d9714cead174c70ecd5e0e1daf7394a22
uJnaE <cjpoledo26@gmail.com>
2024-01-02 19:32:14 +0800

    added sahi results

--- optimal_overlap_calcu.py
+++ optimal_overlap_calcu.py
@@ -2,6 +2,7 @@ import os
 import xml.etree.ElementTree as ET
 import math
 
+
 def optimize_overlap(label_folder, target_crop_w, target_crop_h):
     files = os.listdir(label_folder)
 
@@ -11,17 +12,17 @@ def optimize_overlap(label_folder, target_crop_w, target_crop_h):
     all_h = list()
 
     for file in files:
-        if file.endswith('.xml'):
+        if file.endswith(".xml"):
             # read xml and calculate width and height of objects
             tree = ET.parse(os.path.join(label_folder, file))
             root = tree.getroot()
 
-            for object in tree.findall('object'):
-                min_x = int(object.find('bndbox').find('xmin').text)
-                min_y = int(object.find('bndbox').find('ymin').text)
-                max_x = int(object.find('bndbox').find('xmax').text)
-                max_y = int(object.find('bndbox').find('ymax').text)
-            
+            for object in tree.findall("object"):
+                min_x = int(object.find("bndbox").find("xmin").text)
+                min_y = int(object.find("bndbox").find("ymin").text)
+                max_x = int(object.find("bndbox").find("xmax").text)
+                max_y = int(object.find("bndbox").find("ymax").text)
+
             curr_w = max_x - min_x + 1
             curr_h = max_y - min_y + 1
 
@@ -29,7 +30,7 @@ def optimize_overlap(label_folder, target_crop_w, target_crop_h):
                 max_w = curr_w
             if max_h < curr_h:
                 max_h = curr_h
-            
+
             all_w.append(curr_w)
             all_h.append(curr_h)
 
@@ -39,17 +40,21 @@ def optimize_overlap(label_folder, target_crop_w, target_crop_h):
     optimal_overlap_h = max_h / 2 / target_crop_h * 100
     print(f"\n>>> Optimal overlap for {target_crop_w} x {target_crop_h} crop\n")
     print(f"===== Based on largest width({max_w}) and height({max_h}) =====")
-    print('width overlap:', math.ceil(optimal_overlap_w) / 100)
-    print('height overlap:', math.ceil(optimal_overlap_h) / 100)
+    print("width overlap:", math.ceil(optimal_overlap_w) / 100)
+    print("height overlap:", math.ceil(optimal_overlap_h) / 100)
     ave_w = sum(all_w) / len(all_w)
     ave_h = sum(all_h) / len(all_h)
     optimal_overlap_w = ave_w / 2 / target_crop_w * 100
     optimal_overlap_h = ave_h / 2 / target_crop_h * 100
-    print(f"\n===== Based on average width({round(ave_w)}) and height({round(ave_h)}) =====")
-    print('width overlap:', math.ceil(optimal_overlap_w) / 100)
-    print('height overlap:', math.ceil(optimal_overlap_h) / 100)
+    print(
+        f"\n===== Based on average width({round(ave_w)}) and height({round(ave_h)}) ====="
+    )
+    print("width overlap:", math.ceil(optimal_overlap_w) / 100)
+    print("height overlap:", math.ceil(optimal_overlap_h) / 100)
+
 
 # note to future self, accept objects that are still 75% whole after crop to remove dupes while minimizing crops for a bit faster training
-optimize_overlap("C:/Users/Lenovo ThinkBook/Documents/Thesis/heridal/trainImages/labels/", 320, 320)
-optimize_overlap("C:/Users/Lenovo ThinkBook/Documents/Thesis/heridal/trainImages/labels/", 640, 640)
-optimize_overlap("C:/Users/Lenovo ThinkBook/Documents/Thesis/heridal/trainImages/labels/", 512, 512)
+# optimize_overlap("C:/Users/Lenovo ThinkBook/Documents/Thesis/heridal/trainImages/labels/", 320, 320)
+# optimize_overlap("C:/Users/Lenovo ThinkBook/Documents/Thesis/heridal/trainImages/labels/", 640, 640)
+# optimize_overlap("C:/Users/Lenovo ThinkBook/Documents/Thesis/heridal/trainImages/labels/", 512, 512)
+optimize_overlap("D:/00FILES/Thesis2/heridal/testImages/labels", 1280, 1280)
